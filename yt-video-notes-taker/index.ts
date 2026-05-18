80b961319d003d285c274a4a5389e8e8b9173b86
Shivpratap <shivpratap05oct2007@gmail.com>
2026-05-18 13:28:05 +0530

    note taker is complete atleast v1 if any other modification we will do later now focus on the lectures

--- yt-video-notes-taker/index.ts
+++ yt-video-notes-taker/index.ts
@@ -1,25 +1,35 @@
 import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
+import readline from "readline/promises";
 
 dotenv.config();
 
 import { llm } from "./llm.ts";
 import { SYSTEM_PROMPT } from "./prompt.ts";
 import yt_transcript from "./yt_transcript_get.ts";
+import { stdin } from "process";
 
-async function run(url: string) {
-  const outputDir = path.resolve(__dirname, "./output");
+async function main(url: string, lecture_num: number) {
+  console.log("Extracting transcript");
+
+  const transcript = await yt_transcript(url);
+  console.log("--Making Notes--");
+  console.log("Wait for a second");
+
+  const outputDir = path.resolve(import.meta.dirname, `../lec-${lecture_num}`);
+
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir, { recursive: true });
   }
-  const filePath = path.resolve(outputDir, "lec-1.md");
+  const filePath = path.resolve(outputDir, `notes.md`);
 
   const writeStream = fs.createWriteStream(filePath, { encoding: "utf8" });
+
   try {
     const response = await llm({
-      systemPrompt: "you are helpful assistant",
-      query: "Hello, who are you.", 
+      systemPrompt: SYSTEM_PROMPT,
+      query: transcript,
       model: "moonshotai/kimi-k2.6",
       stream: true,
       temperature: 0.15,
@@ -27,7 +37,7 @@ async function run(url: string) {
     });
     for await (const chunk of response) {
       writeStream.write(chunk);
-      process.stdout.write(chunk)
+      process.stdout.write(chunk);
     }
   } catch (error) {
     console.error("An error occurred during LLM stream:", error);
@@ -40,4 +50,35 @@ async function run(url: string) {
   });
 }
 
-await run("https://youtu.be/jGwO_UgTS7I?si=GYJjut2i_NCY6WD1");
+async function run() {
+  const ytRegex =
+    /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|shorts\/|live\/|watch\?(?:.*&)?v=))([a-zA-Z0-9_-]{11})(?:\S+)?$/;
+  const args = process.argv.slice(2);
+
+  if (args.length < 2 || args.length > 2) {
+    console.log(`Error: Please provide exactly 2 inputs.
+<url> <lecture number>
+        `);
+    return;
+  }
+
+  const [url, lec_no] = args;
+
+  if (!ytRegex.test(url)) {
+    console.log("Please enter youtube video url.");
+    return;
+  }
+
+  try {
+    const lec = Number(lec_no);
+    if (url && lec) {
+      console.log("--Started--");
+
+      await main(url, lec);
+    }
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+}
+
+run();
